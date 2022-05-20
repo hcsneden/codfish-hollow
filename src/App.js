40f068c463f6fcb42fd4edeaecb43fae419224aa
hcsneden <48335502+hcsneden@users.noreply.github.com>
2022-05-19 21:33:19 -0500

    Revert "Updates"
    This reverts commit 328711996b81226ef9a3d76b8e0a5359445b2567.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+import HeroImage from './components/HeroImage'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <HeroImage></HeroImage>
+    </div>
+  );
+}
+
+export default App;
