a92fa148234f9a878c1aa60f31479274107abd8c
hcsneden <48335502+hcsneden@users.noreply.github.com>
2022-05-26 13:32:09 -0500

    checking in routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import Header from './components/Header';
 import HeroImage from './components/HeroImage';
 import UpcomingShows from './components/UpcomingShows'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+//import About from './components/About'
 
 function App() {
 
   return (
     <div className="App">
-      <Header></Header>
-      <HeroImage></HeroImage>
-      <UpcomingShows></UpcomingShows>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/codfish-hollow" element={<><Header/><HeroImage/><UpcomingShows/></>}>
+          </Route>
+          <Route path="/codfish-hollow/shows" element={<UpcomingShows/>}>
+          </Route>
+          {/* <Route path="/codfish-hollow/about" element={<About/>}>
+          </Route> */}
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
