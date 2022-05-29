63b61fd03e7f870021a853fe776875f3a2042ad5
Arianna Joy Buller <45601428+AriannaBuller@users.noreply.github.com>
2022-05-29 12:20:52 -0500

    Create FAQ page

---
+++ src/components/FAQ.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+
+const FAQ = () => {
+    return (
+
+
+    )
+}
+
+export default FAQ;
