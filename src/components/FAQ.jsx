e977264530661b16c5d3b24279c9543db5bcf84d
Arianna Joy Buller <45601428+AriannaBuller@users.noreply.github.com>
2022-05-29 15:31:26 -0500

    updates to faq page

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,53 +1,51 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
+// import { Col, Container, Row } from "react-bootstrap";
+// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { CardGroup } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
 
 const FAQ = () => {
     return (
-        <CardGroup>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This card has supporting text below as a natural lead-in to additional
-                        content.{' '}
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This card has even longer content than the first to
-                        show that equal height action.
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-        </CardGroup>
+
+    <CardGroup>
+        <Card>
+            <Card.Img variant="top" src={require("../images/concessions.PNG")} />
+            <Card.Header>Concessions</Card.Header>
+            <ListGroup variant="flush">
+                <ListGroup.Item>Do you sell alcohol?</ListGroup.Item>
+                <ListGroup.Item>Do you sell food and drink?</ListGroup.Item>
+                <ListGroup.Item>Do vendors accept credit cards?</ListGroup.Item>
+            </ListGroup>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Header>Ticket Sales</Card.Header>
+            <ListGroup variant="flush">
+                <ListGroup.Item>Concert is sold out?</ListGroup.Item>
+                <ListGroup.Item>Can't attend show I bought tickets for?</ListGroup.Item>
+                <ListGroup.Item>Do you accept credit cards at the gate?</ListGroup.Item>
+            </ListGroup>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Header>Parking/Seating</Card.Header>
+            <ListGroup variant="flush">
+                <ListGroup.Item>Can I camp?</ListGroup.Item>
+                <ListGroup.Item>Is there seating?</ListGroup.Item>
+                <ListGroup.Item>Is there reserved parking?</ListGroup.Item>
+            </ListGroup>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Header>Policy/Etiquette</Card.Header>
+            <ListGroup variant="flush">
+                <ListGroup.Item>Can I bring my pet?</ListGroup.Item>
+                <ListGroup.Item>Can I smoke in the barn?</ListGroup.Item>
+                <ListGroup.Item>Can I talk loudly during songs?</ListGroup.Item>
+            </ListGroup>
+        </Card>
+    </CardGroup>
 
     )
 }
