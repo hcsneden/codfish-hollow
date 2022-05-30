03b9f812127a869d171945f60a06b5b4382cfb28
Arianna Joy Buller <45601428+AriannaBuller@users.noreply.github.com>
2022-05-30 14:22:00 -0500

    added first according drop down to concessions

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -3,6 +3,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { ListGroup } from "react-bootstrap";
+import { Accordion } from "react-bootstrap";
 
 
 const FAQ = () => {
@@ -10,7 +11,7 @@ const FAQ = () => {
         <div>
             <img
                 className="faq-hero-image"
-                src={require("../images/image1.png")}
+                src={require("../images/band playing.jpg")}
                 alt="First slide"
             />
             <div className="faq-banner">
@@ -23,11 +24,30 @@ const FAQ = () => {
                     <Col md={6}>
                         <Card>
                             <Card.Img className="faq-card" variant="top" src={require("../images/concessions.PNG")} />
-                            <ListGroup variant="flush">
-                                <ListGroup.Item>Do you sell alcohol?</ListGroup.Item>
-                                <ListGroup.Item>Do you sell food and drink?</ListGroup.Item>
-                                <ListGroup.Item>Do vendors accept credit cards?</ListGroup.Item>
-                            </ListGroup>
+                            <Accordion defaultActiveKey="0">
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header>Do you sell alcohol?</Accordion.Header>
+                                    <Accordion.Body>
+                                        Yes! We sell a few types of can beer and some bottled beer and wine. No outside coolers allowed.
+                                        You may still bring your coolers to the camping area/parking lot, but no coolers or outside drinks
+                                        on the hay rack or past the ticket area. You must bring a photo ID if you would like to purchase alcohol.
+                                        Cash only, we do not accept credit/debit cards or checks.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="1">
+                                    <Accordion.Header>Do you sell food and drink?</Accordion.Header>
+                                    <Accordion.Body>
+                                        Yes! We have a food truck at every show, sometimes two!
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="2">
+                                    <Accordion.Header>Do vendors accept credit cards?</Accordion.Header>
+                                    <Accordion.Body>
+                                        Unfortunately we only accept cash payment. There is an ATM at the barn but sometimes the internet
+                                        is spotty so it is best to bring cash.
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
                             <Button variant="secondary">More Questions</Button>
                         </Card>
                     </Col>
